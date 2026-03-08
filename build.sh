bcd2ed4511185147b493fae89fd6516201d6fc5d
meowjiao <meowjiao321@163.com>
2026-03-08 18:58:54 +0800

    新增 EggAI（代码由 AI 编写）

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
-export TOOLCHAIN=$HOME/toolchain
+#!/bin/bash
+
+make clean
+
+export TOOLCHAIN=/opt/musl-v833
 export PATH=$PATH:$TOOLCHAIN/bin
-export CC=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-gcc
-export CXX=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-g++
-export AR=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-ar
-export RANLIB=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-ranlib
-export STRIP=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-strip
+export CC=$TOOLCHAIN/bin/arm-linux-musleabihf-gcc
+export CXX=$TOOLCHAIN/bin/arm-linux-musleabihf-g++
+export AR=$TOOLCHAIN/bin/arm-linux-musleabihf-ar
+export RANLIB=$TOOLCHAIN/bin/arm-linux-musleabihf-ranlib
+export STRIP=$TOOLCHAIN/bin/arm-linux-musleabihf-strip
 
 export STAGING_DIR=$TOOLCHAIN/bin
 
