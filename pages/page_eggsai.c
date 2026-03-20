6e519337d05c9bc55adf970ab7fe4629e1366838
Xiaoyiawa <1079802404@qq.com>
2026-03-20 20:20:36 +0800

    为ai页面做了些排版优化

--- pages/page_eggsai.c
+++ pages/page_eggsai.c
@@ -15,30 +15,6 @@
 #include "page_eggsai.h"
 
 /*-----------------------------------------------------
- *  Layout constants (pixel values for 240×240)
- *----------------------------------------------------*/
-#define SCREEN_W 240
-#define SCREEN_H 240
-
-#define TOP_BAR_H 30
-#define BOTTOM_BAR_H 34
-#define GAP 2
-#define KB_H 110
-
-/* Normal (keyboard hidden) positions */
-#define CHAT_Y (TOP_BAR_H + GAP)                       /* 32 */
-#define BOTTOM_Y_NORMAL (SCREEN_H - BOTTOM_BAR_H)      /* 206 */
-#define CHAT_H_NORMAL (BOTTOM_Y_NORMAL - GAP - CHAT_Y) /* 172 */
-
-/* Keyboard-shown positions */
-#define KB_Y (SCREEN_H - KB_H)                 /* 130 */
-#define BOTTOM_Y_KB (KB_Y - BOTTOM_BAR_H)      /* 96 */
-#define CHAT_H_KB (BOTTOM_Y_KB - GAP - CHAT_Y) /* 62 */
-
-#define BACK_BTN_W 30
-#define SEND_BTN_W 50
-
-/*-----------------------------------------------------
  *  Static state
  *----------------------------------------------------*/
 static EggsaiProvider * s_provider = NULL;
